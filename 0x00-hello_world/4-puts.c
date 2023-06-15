9d34e412da1eabcc781c08b80390f8760ee900d7
AnyumbaKE <esanyumba@gmail.com>
2023-06-15 14:48:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 *main - entry point
-*return : always  0
+*return : always 0
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzl");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
