149f65cc28c8b1dea8d060c9c689647f7cffaa55
AnyumbaKE <esanyumba@gmail.com>
2023-06-16 15:45:05 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns combo
+ *
+ * Return: return 0 if successful
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			if (x < y)
+			{
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
