3d61c2fe4726a9f9f10fde46bdaeb4da4ca49e1d
AnyumbaKE <esanyumba@gmail.com>
2023-06-20 18:23:19 -0700

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * values <= 4,000,000
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int x = 0, y = 1, z = 0;
+	int sum = 0;
+
+	while (next < 4000000)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		if (z % 2 == 0)
+			sum += z;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
