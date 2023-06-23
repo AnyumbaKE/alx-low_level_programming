685e3b3a34a6738a0e5e03ec99f684780e0f8505
AnyumbaKE <esanyumba@gmail.com>
2023-06-22 17:29:37 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers
+ * Return: 0 Always (Success)
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
