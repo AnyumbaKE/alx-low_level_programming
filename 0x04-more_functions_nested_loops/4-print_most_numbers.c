87109c08c736a9ccccd63348fa1e81e7106be36a
AnyumbaKE <esanyumba@gmail.com>
2023-06-22 19:35:19 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		if (x == 2 && x == 4)
+		if (x != 2 && x != 4)
 		{
-			_putchar(x);
+			_putchar(x + '0');
 		}
 	}
 		_putchar('\n');
