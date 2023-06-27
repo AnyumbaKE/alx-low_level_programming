98a9bbac508cfa63e0a7149c0f5867aba08b4169
AnyumbaKE <esanyumba@gmail.com>
2023-06-27 07:34:20 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * reset_to_98 - updates the value it points to to 98
+  * @n: is the parameter int
+  * Return: Always 0 (Success)
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
