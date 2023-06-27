23e3877badca652b83766917fe0b5903ed4bb681
AnyumbaKE <esanyumba@gmail.com>
2023-06-27 14:30:15 -0700

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - a program that generates random valid passwords
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+
+	printf("%c", 2772 - sum);
+
+	return (0);
+}
