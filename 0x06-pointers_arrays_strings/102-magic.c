6cfceffd690e283a7af77385060d76281684fd95
AnyumbaKE <esanyumba@gmail.com>
2023-06-29 15:39:26 -0700

    10.Dreams

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a[5];
+	int *p;
+	int x;
+
+	a[2] = 1024;
+	p = &n;
+
+	*(p + 5) = 98;
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
