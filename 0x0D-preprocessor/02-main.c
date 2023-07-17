0f833278c41a221d3d89266f9589c1e5c84d41ac
AnyumbaKE <esanyumba@gmail.com>
2023-07-17 10:34:12 -0700

    2. File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - prints the name of the file it was compiled
+  * Return: 0 Always (Success)
+  **/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
