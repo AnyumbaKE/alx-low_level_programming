3d86c0642ba0ad15548d7648c3aec364a82a8c9a
AnyumbaKE <esanyumba@gmail.com>
2023-07-19 10:55:31 -0700

    2. To hell with circumstances

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,16 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x;
 
-	if (size <= 0)
+	if (array != NULL && cmp != NULL)
 	{
-		return (-1);
-	}
-
-	for (x = 0; x < size; ++x)
-	{
-		if (cmp(array[x]))
+		for (x = 0; x < size; x++)
 		{
-			return (x);
+			if (cmp(array[x]) != 0)
+				return (x);
 		}
 	}
 
