1fc36915b080b0a9e2a5849566e6c9c2714dff8f
AnyumbaKE <esanyumba@gmail.com>
2023-09-18 08:53:00 -0700

    abs

--- 0x18-dynamic_libraries/abs.c
+++ 0x18-dynamic_libraries/abs.c
@@ -1,17 +1,19 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- *  * _abs - computes the absolute value of an integer
- *   * @n: The int to be operated upon
- *     * Return: returns an unsinged int value to calling function
- *      */
+ * _abs - a function that computes the absolute value of an integer
+ * @n: The character to be checked
+ * Return: unsigned int value
+ */
 int _abs(int n)
 {
-		if (n > 0)
-					return (n);
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n *= -1;
+		return (n);
 
-			else
-					{
-								n *= -1;
-										return (n);
-											}
-}
