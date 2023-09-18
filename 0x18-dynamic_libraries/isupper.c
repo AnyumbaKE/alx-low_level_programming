c606fd182cf781bd8fe8594104c5c598f1743a4b
AnyumbaKE <esanyumba@gmail.com>
2023-09-18 08:54:59 -0700

    isupper

---
+++ 0x18-dynamic_libraries/isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ * @c: the character
+ * Return: 0 Always (Success)
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
