db91100b1e87cc85698cd34fdda7a399b3e9a330
AnyumbaKE <esanyumba@gmail.com>
2023-10-06 14:21:03 -0700

    4. >>> ht[betty]

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,19 +9,17 @@
  * @key: key
  * Return: value or NULL if not found
  **/
-char *hash_table_get(const table_t *ht, const char *key)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	ulint idx;
-	node_t *tmp;
+	hash_node_t *tmp;
+	unsigned long int x;
 
-	/* find index in hash table where key is */
-	/* look through linked list to find matching key for value */
 
 	if (!ht || !key)
 		return (NULL);
-	idx = key_index((const unsigned char *)key, ht->size);
+	x = key_index((const unsigned char *)key, ht->size);
 
-	tmp = (ht->array)[idx];
+	tmp = (ht->array)[x];
 	while (tmp != NULL && strcmp(tmp->key, key) != 0)
 		tmp = tmp->next;
 	if (!tmp)
