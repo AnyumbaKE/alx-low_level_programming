35b21193c127ae466997b8adf4e205c8c7a47d3a
AnyumbaKE <esanyumba@gmail.com>
2023-10-06 13:48:41 -0700

    hash.h

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,19 +11,21 @@
  **/
 char *hash_table_get(const table_t *ht, const char *key)
 {
-	hash_node_t *node;
-	unsigned long int index;
+	ulint idx;
+	node_t *tmp;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	/* find index in hash table where key is */
+	/* look through linked list to find matching key for value */
+
+	if (!ht || !key)
 		return (NULL);
+	idx = key_index((const unsigned char *)key, ht->size);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
+	tmp = (ht->array)[idx];
+	while (tmp != NULL && strcmp(tmp->key, key) != 0)
+		tmp = tmp->next;
+	if (!tmp)
 		return (NULL);
-
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
-
-	return ((node == NULL) ? NULL : node->value);
+	else
+		return (tmp->value);
 }
